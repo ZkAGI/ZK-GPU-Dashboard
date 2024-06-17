342d1fd835fb1b6431f2718d95f0149481935570
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-06-17 16:33:00 +0530

    ♻️ Refactor code: API Integration

--- components/dashboard/UtilizationInfo.tsx
+++ components/dashboard/UtilizationInfo.tsx
@@ -5,9 +5,9 @@ const UtilizationInfo: React.FC = () => {
     <div className='flex flex-row justify-between px-2'>
       <div>
         <div>Utilization Overview</div>
-        <div className='text-gray-400 text-xs'>
+        {/* <div className='text-gray-400 text-xs'>
           <span className='text-green-500'>+5 more</span> in last 7 days
-        </div>
+        </div> */}
       </div>
       {/* <div className='grid grid-cols-3 gap-2 justify-center items-center'>
         <div className='bg-[#1E2056] px-2 py-1 text-center rounded text-xs'>Days</div>
