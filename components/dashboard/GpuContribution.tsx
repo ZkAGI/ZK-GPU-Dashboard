36d2033c10746984537f461c773a8a21258c1cfa
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-10-08 13:51:37 +0530

    [UI] GPU contribution radial graph display logic updated
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- components/dashboard/GpuContribution.tsx
+++ components/dashboard/GpuContribution.tsx
@@ -8,19 +8,34 @@ interface Node {
   node_ids: string[];
 }
 
+interface IpNodesMap {
+  [key: string]: string[];
+}
+
+interface NodeGpuMap {
+  [key: string]: GpuInfo;
+}
+
+interface GpuInfo {
+  names: string[];
+  totalMemory: number;
+}
+
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const KEY = process.env.API_KEY;
 
 const GPUContribution: React.FC = () => {
   const { wallet } = useWallet();
   const walletAddress = wallet?.adapter?.publicKey?.toString();
-  const [list, setList] = useState<Node>({ node_ids: [] });
-  const [selectedIp, setSelectedIp] = useState<string>('');
+  const [ipNodesMap, setIpNodesMap] = useState<IpNodesMap>({});
+  const [totalNodeIds, setTotalNodeIds] = useState<string[]>([]);
   const { data: nodesData, error: nodesError } = useSWR(`${BASE_URL}/api/nodes`, { refreshInterval: 1000 });
   const { data: gpuData, error: gpuError } = useSWR(`${BASE_URL}/api/dailystats`, { refreshInterval: 8000 });
   const [aliveCount, setAliveCount] = useState(0);
   const [gpuCount, setGpuCount] = useState(0);
   const [gpuNames, setGpuNames] = useState<{ [key: string]: string }>({});
+  const [nodeGpuMap, setNodeGpuMap] = useState<NodeGpuMap>({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,26 +48,39 @@ const GPUContribution: React.FC = () => {
             "api-key": `${KEY}`
           },
         });
+
         if (response.status === 200) {
-          const ipResponse = await axios({
-            method: 'GET',
-            url: `${BASE_URL}/ips/${response.data.ip_addresses[0]}/nodes`,
-            headers: {
-              'Content-Type': 'application/json',
-              'api-key': `${KEY}`,
-            },
-          });
-          if (ipResponse.status === 200) {
-            const filteredNodeIds = ipResponse.data.node_ids.filter((nodeId: string) => {
-              const node = nodesData?.data?.summary.find((node: any) => node.raylet.nodeId === nodeId);
-              return node && node.raylet.state !== 'DEAD';
-            });
-            setList({ node_ids: filteredNodeIds });
-            setSelectedIp(response.data.ip_addresses[0]);
-          } else {
-            setList({ node_ids: [] });
-            setSelectedIp('');
-          }
+          const ipAddresses = response.data.ip_addresses;
+          const newIpNodesMap: IpNodesMap = {};
+          let allNodeIds: string[] = [];
+
+          // Fetch nodes for each IP address
+          await Promise.all(ipAddresses.map(async (ipAddress: string) => {
+            try {
+              const ipResponse = await axios({
+                method: 'GET',
+                url: `${BASE_URL}/ips/${ipAddress}/nodes`,
+                headers: {
+                  'Content-Type': 'application/json',
+                  'api-key': `${KEY}`,
+                },
+              });
+
+              if (ipResponse.status === 200) {
+                const filteredNodeIds = ipResponse.data.node_ids.filter((nodeId: string) => {
+                  const node = nodesData?.data?.summary.find((node: any) => node.raylet.nodeId === nodeId);
+                  return node && node.raylet.state !== 'DEAD';
+                });
+                newIpNodesMap[ipAddress] = filteredNodeIds;
+                allNodeIds = [...allNodeIds, ...filteredNodeIds];
+              }
+            } catch (error) {
+              console.error(`Error fetching nodes for IP ${ipAddress}:`, error);
+            }
+          }));
+
+          setIpNodesMap(newIpNodesMap);
+          setTotalNodeIds(allNodeIds);
         }
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -72,56 +100,62 @@ const GPUContribution: React.FC = () => {
           }
           return totalMemory;
         }, 0);
-
         setAliveCount(Math.floor(totalGpuMemory));
       }
     }
   }, [nodesData]);
 
   useEffect(() => {
-    if (nodesData && selectedIp) {
+    if (nodesData && Object.keys(ipNodesMap).length > 0) {
       const clusterData = nodesData?.data?.summary;
       if (clusterData) {
         const fetchGpuNames = async () => {
+          let totalMemory = 0;
+          const newNodeGpuMap: NodeGpuMap = {};
+
           await Promise.all(
-            clusterData.map(async (node: any) => {
-              if (node.ip === selectedIp) {
-                try {
-                  const response = await axios({
-                    method: 'GET',
-                    url: `${BASE_URL}/nodes/${node.raylet.nodeId}/active-time`,
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'api-key': `${KEY}`,
-                    },
-                  });
-                  if (response.status === 200) {
-                    const gpuName = node.gpus?.[0]?.name || 'Unknown GPU';
-                    setGpuNames((prevNames) => ({ ...prevNames, [node.raylet.nodeId]: gpuName }));
-                  } else {
-                    console.log('Error fetching node active time', response);
+            Object.entries(ipNodesMap).flatMap(([ip, nodeIds]) =>
+              nodeIds.map(async (nodeId) => {
+                const node = clusterData.find((n: any) => n.raylet.nodeId === nodeId);
+                if (node && node.raylet.state !== 'DEAD') {
+                  try {
+                    const response = await axios({
+                      method: 'GET',
+                      url: `${BASE_URL}/nodes/${nodeId}/active-time`,
+                      headers: {
+                        'Content-Type': 'application/json',
+                        'api-key': `${KEY}`,
+                      },
+                    });
+                    if (response.status === 200) {
+                      const gpuNames = node.gpus?.map((gpu: any) => gpu.name || 'Unknown GPU') || ['Unknown GPU'];
+                      const nodeMemory = Array.isArray(node.gpus)
+                        ? node.gpus.reduce((gpuTotal: number, gpu: any) =>
+                          gpuTotal + (gpu?.memoryTotal ? Number(gpu.memoryTotal) / 1024 : 0), 0)
+                        : 0;
+
+                      newNodeGpuMap[nodeId] = {
+                        names: gpuNames,
+                        totalMemory: nodeMemory
+                      };
+                      totalMemory += nodeMemory;
+                    }
+                  } catch (error) {
+                    console.error('Error fetching GPU names', error);
                   }
-                } catch (error) {
-                  console.error('Error fetching GPU names', error);
                 }
-              }
-            })
+              })
+            )
           );
 
-          const totalGpuMemory = clusterData.reduce((totalMemory: number, node: any) => {
-            if (node.ip === selectedIp && node.raylet.state !== 'DEAD') {
-              return totalMemory + (Array.isArray(node.gpus) ? node.gpus.reduce((gpuTotal: number, gpu: any) => gpuTotal + (gpu?.memoryTotal ? Number(gpu.memoryTotal) / 1024 : 0), 0) : 0);
-            }
-            return totalMemory;
-          }, 0);
-
-          setGpuCount(totalGpuMemory);
+          setNodeGpuMap(newNodeGpuMap);
+          setGpuCount(totalMemory);
         };
 
         fetchGpuNames();
       }
     }
-  }, [nodesData, selectedIp]);
+  }, [nodesData, ipNodesMap]);
 
   return (
     <div>
@@ -131,10 +165,16 @@ const GPUContribution: React.FC = () => {
           <RadialBarChart aliveCount={aliveCount} gpuCount={gpuCount} />
         </div>
         <div className="w-full p-4 text-xs rounded-xl bg-[#171D3D] mb-0.5">
-          {list.node_ids.length > 0 ? (
-            list.node_ids.map((nodeId, index) => (
-              <div key={index}>{gpuNames[nodeId] || 'Unknown GPU'}</div>
-            ))
+          {Object.values(nodeGpuMap).flatMap(node => node.names).length > 0 ? (
+            <div>
+              {Object.values(nodeGpuMap).flatMap(node =>
+                node.names.map((gpuName, index) => (
+                  <div key={index} className="mb-1">
+                    • {gpuName}
+                  </div>
+                ))
+              )}
+            </div>
           ) : (
             <div className="flex justify-center items-center text-center">You have no GPUs yet!</div>
           )}
@@ -145,4 +185,3 @@ const GPUContribution: React.FC = () => {
 };
 
 export default GPUContribution;
-
